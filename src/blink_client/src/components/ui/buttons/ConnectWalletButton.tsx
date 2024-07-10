7f4aad0a2b2e58dc5aa1a3549d4983d67f53e8fc
Sam <samuel.barnades@gmail.com>
2024-07-10 18:43:06 +0100

    Error, have to pull before.

--- src/blink_client/src/components/ui/buttons/ConnectWalletButton.tsx
+++ src/blink_client/src/components/ui/buttons/ConnectWalletButton.tsx
@@ -5,12 +5,12 @@ const ConnectWalletButton = ({
   handleConnect, 
 }: { 
   handleConnect: () => void; 
-}) => (
-  <div className="rounded-full text-center p-2 m-2 bg-gradient-to-r from-lime-300 to-green-500 transition ease-in delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-150">  
+}) => ( 
   <button 
     className="text-stone-900 font-medium" 
     onClick={() => handleConnect()} 
   > 
+    <div className="rounded-full text-center p-2 m-2 bg-gradient-to-r from-lime-300 to-green-500 transition ease-in delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-150"> 
       <span className="inline-flex items-center"> 
         <img 
           className="pr-2" 
@@ -20,8 +20,8 @@ const ConnectWalletButton = ({
         /> 
         <p className="pl-2">Connect Wallet</p> 
       </span> 
-  </button>
-  </div>
+    </div> 
+  </button> 
 ); 
  
 export default ConnectWalletButton;
