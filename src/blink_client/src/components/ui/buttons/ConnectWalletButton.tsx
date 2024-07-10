975d5e4aae27dd5d2bf600389e095fe41c9637b6
Sam <samuel.barnades@gmail.com>
2024-07-10 19:26:33 +0100

    Resolve the problem of the router & pushing the button wallet desing.

--- src/blink_client/src/components/ui/buttons/ConnectWalletButton.tsx
+++ src/blink_client/src/components/ui/buttons/ConnectWalletButton.tsx
@@ -1,27 +1,27 @@
-import React from "react"; 
-import stoicWalletLogo from "../../../assets/stoic-wallet-logo.png"; 
- 
-const ConnectWalletButton = ({ 
-  handleConnect, 
-}: { 
-  handleConnect: () => void; 
-}) => ( 
-  <button 
-    className="text-stone-900 font-medium" 
-    onClick={() => handleConnect()} 
-  > 
-    <div className="rounded-full text-center p-2 m-2 bg-gradient-to-r from-lime-300 to-green-500 transition ease-in delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-150"> 
-      <span className="inline-flex items-center"> 
-        <img 
-          className="pr-2" 
-          src={stoicWalletLogo} 
-          width={75} 
-          alt="Stoic Wallet" 
-        /> 
-        <p className="pl-2">Connect Wallet</p> 
-      </span> 
-    </div> 
-  </button> 
-); 
- 
+import React from "react";
+import stoicWalletLogo from "../../../assets/stoic-wallet-logo.png";
+
+const ConnectWalletButton = ({
+  handleConnect,
+}: {
+  handleConnect: () => void;
+}) => (
+  <div className="rounded-full text-center p-2 m-2 bg-gradient-to-r from-lime-300 to-green-500 transition ease-in delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-150">
+    <button
+      className="text-stone-900 font-medium"
+      onClick={() => handleConnect()}
+    >
+      <span className="inline-flex items-center">
+        <img
+          className="pr-2"
+          src={stoicWalletLogo}
+          width={75}
+          alt="Stoic Wallet"
+        />
+        <p className="pl-2">Connect Wallet</p>
+      </span>
+    </button>
+  </div>
+);
+
 export default ConnectWalletButton;
